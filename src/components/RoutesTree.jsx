3f2eb1f00e90ce63143bd3e1dd14bf05395627ed
Jordi <jordipelaez05@gmail.com>
2023-10-05 17:24:40 +0200

    routing

--- src/components/RoutesTree.jsx
+++ src/components/RoutesTree.jsx
@@ -1,15 +1,12 @@
 import React from "react";
 import { createHashRouter, RouterProvider } from "react-router-dom";
-import Info from "./components/Info";
-import AboutMe from "./components/AboutMe";
-import Projects from "./components/Projects";
-
-
-
+import Info from "./Info";
+import AboutMe from "./AboutMe";
+import Projects from "./Projects";
 
 const router = createHashRouter([
     {
-      path: "/",
+      path: "/Info",
       element: <Info />
     },
     {
@@ -18,14 +15,13 @@ const router = createHashRouter([
     },
     {
       path: "/Projects",
-      element: <Projects/ >
+      element: <Projects />
     }
   ]);
 
 const RoutesTree = () => {
     return(
         <RouterProvider router = {router}/>
-
     );
 };
 export default RoutesTree;
