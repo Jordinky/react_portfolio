90fd147d892b5df7ed2909ef9c6cacb71f381446
Jordi <jordipelaez05@gmail.com>
2023-10-05 16:53:28 +0200

    implementing hashrouter

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
+
 import reportWebVitals from "./reportWebVitals";
 import { createHashRouter, RouterProvider } from "react-router-dom";
 import Info from "./components/Info";
@@ -30,7 +30,7 @@ const router = createHashRouter([
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <RouterProvider />
+    <RouterProvider router = {router} />
   </React.StrictMode>
 );
 
