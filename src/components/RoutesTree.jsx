52ce40cf4334f1520c4f43f33b0647b1fdabbf6b
Jordi <jordipelaez05@gmail.com>
2023-10-05 17:04:50 +0200

    more routing

--- src/components/RoutesTree.jsx
+++ src/components/RoutesTree.jsx
@@ -1,19 +1,19 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
 import Info from "./Info";
 import Projects from "./Projects";
 import AboutMe from "./AboutMe";
-import { HashRouter } from "react-router-dom";
+
 
 const RoutesTree = () => {
     return(
-        <HashRouter>
+        <Router>
             <Routes>
                 <Route path ="/Info" element = {<Info />}/>
                 <Route path ="/Projects" element = {<Projects />}/>
                 <Route path ="/AboutMe" element = {<AboutMe />}/>  
             </Routes>
-        </HashRouter>
+        </Router>
     );
 };
 export default RoutesTree;
