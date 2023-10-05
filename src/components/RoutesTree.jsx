d0f0cb93e8d347e38e33d410a55668df3483e61f
Jordi <jordipelaez05@gmail.com>
2023-10-05 16:12:53 +0200

    Changed routing from browserouter to hashrouter

--- src/components/RoutesTree.jsx
+++ src/components/RoutesTree.jsx
@@ -1,19 +1,19 @@
 import React from "react";
-import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Info from "./Info";
 import Projects from "./Projects";
 import AboutMe from "./AboutMe";
-
+import { HashRouter } from "react-router-dom";
 
 const RoutesTree = () => {
     return(
-        <Router>
+        <HashRouter>
             <Routes>
                 <Route path ="/Info" element = {<Info />}/>
                 <Route path ="/Projects" element = {<Projects />}/>
                 <Route path ="/AboutMe" element = {<AboutMe />}/>  
             </Routes>
-        </Router>
+        </HashRouter>
     );
 };
 export default RoutesTree;
