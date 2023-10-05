d54309385f79cb249ef0b840a70c375b515a977b
Jordi <jordipelaez05@gmail.com>
2023-10-05 16:00:46 +0200

    url testing

--- src/info.jsx
+++ src/info.jsx
@@ -9,17 +9,17 @@ export const info = {
         { 
           index: 1,
           text: "home", 
-          link: "./Info",
+          link: "/Info",
         }, 
         { 
           index: 2,
           text: "about me", 
-          link: "./AboutMe",
+          link: "/AboutMe",
         }, 
         { 
           index: 3,
           text: "projects", 
-          link: "./Projects",
+          link: "/Projects",
         },       
     ],
     projectInfo :[
